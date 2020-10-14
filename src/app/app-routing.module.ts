75af7cedfaded5e247bb619b5797d9214650f415
Aaron Boodoo <aaronboodoo@live.com>
2020-10-13 22:34:36 -0400

    Created user information modal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'userinfo',
+    loadChildren: () => import('./userinfo/userinfo.module').then( m => m.UserinfoPageModule)
+  },
 ];
 
 @NgModule({
